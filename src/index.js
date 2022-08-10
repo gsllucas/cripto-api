9ba35ab17e52222e8ba2b58c07051dda618ce8e1
gsllucas <lucas.goncalves@coopersystem.com.br>
2022-08-10 20:13:49 -0300

    :wrench: Added access control headers

--- src/index.js
+++ src/index.js
@@ -12,24 +12,14 @@ app.use(express.json()); // middleware
 
 const routes = require('./routes/cripto-routes');
 
-app.use('/criptos', routes);
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
-  );
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
-app.use(cors({ origin: '*' }));
+app.use('/criptos', routes);
 
 // initial route
 app.get('/', (req, res) => {
